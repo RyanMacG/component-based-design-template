97a8f8b50a5a5ec1ff2d235a90f74867aca10b28
Ryan MacGillivray <rmacg89@gmail.com>
2017-08-18 11:31:56 +0100

    Add circle and progress

---
+++ components/ProgressCircle/index.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { string } from 'prop-types'
+
+const circle = {
+  radius: 60,
+  strokeWidth: 2
+}
+
+export default class Avatar extends React.Component {
+  static propTypes = {
+    progress: string.isRequired,
+    break: string.isRequired,
+  }
+
+  position () {
+    return circle.radius + circle.strokeWidth
+  }
+
+  polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+    const angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+
+    return {
+      x: centerX + (radius * Math.cos(angleInRadians)),
+      y: centerY + (radius * Math.sin(angleInRadians))
+    };
+  }
+
+  describeArc(x, y, radius, startAngle, endAngle){
+    const start = this.polarToCartesian(x, y, radius, endAngle);
+    const end = this.polarToCartesian(x, y, radius, startAngle);
+
+    const largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+
+    const d = [
+        "M", start.x, start.y, 
+        "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y
+    ].join(" ");
+
+    return d;
+  }
+
+  calculateAngle (progress) {
+    return ((progress/100)*360)
+  }
+
+  render () {
+    return (
+      <svg>
+        <circle 
+          cx={this.position()}
+          cy={this.position()}
+          r={circle.radius}
+          stroke="#ffdbb4"
+          strokeWidth={circle.strokeWidth}
+          fill="#aec6cf"
+        />
+        <path fill="none" strokeWidth="38" stroke="#ffdbb4" d={this.describeArc(this.position(), this.position(), circle.radius-20, 0, this.calculateAngle(this.props.progress))}></path>
+      </svg>
+    )
+  }
+}
---
+++ components/ProgressCircle/stories.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import ProgressCircle from './'
+
+storiesOf('ProgressCircle', module)
+  .add('with no progress', () =>
+    <ProgressCircle
+      progress="0"
+      break="0"
+    />
+  )
+  .add('with progress', () =>
+    <ProgressCircle
+      progress="50"
+      break="0"
+    />
+  )
+  .add('with more progress', () =>
+    <ProgressCircle
+      progress="75"
+      break="0"
+    />
+  )
+  .add('with less progress', () =>
+    <ProgressCircle
+      progress="15"
+      break="0"
+    />
+  )
